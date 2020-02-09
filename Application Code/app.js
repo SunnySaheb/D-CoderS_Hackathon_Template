b290db20eec2359ac060f63e8902827015e4b036
Sushobhan Pramanik <sushobhanpramanik@Sushobhans-MacBook-Air.local>
2020-02-09 17:40:50 +0530

    files uploaded

---
+++ Application Code/app.js
@@ -0,0 +1,171 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['tea \nmilk\njam pasta\nmaggi\n','tea\nmilk\nbread butter\nbanana','tea\nmilk\naloo chop\nboiled egg']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['tea \nmilk\njam pasta\nmaggi\n','tea\nmilk\nbread butter\nbanana','tea\nmilk\naloo chop\nboiled egg']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay2').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['tea \nmilk\njam pasta\nmaggi\n','tea\nmilk\nbread butter\nbanana','tea\nmilk\naloo chop\nboiled egg']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay3').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['tea \nmilk\njam pasta\nmaggi\n','tea\nmilk\nbread butter\nbanana','tea\nmilk\naloo chop\nboiled egg']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay4').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['tea \nmilk\njam pasta\nmaggi\n','tea\nmilk\nbread butter\nbanana','tea\nmilk\naloo chop\nboiled egg']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay5').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['tea \nmilk\njam pasta\nmaggi\n','tea\nmilk\nbread butter\nbanana','tea\nmilk\naloo chop\nboiled egg']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay6').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['tea \nmilk\njam pasta\nmaggi\n','tea\nmilk\nbread butter\nbanana','tea\nmilk\naloo chop\nboiled egg']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay7').innerText = menu[ran];  
+})
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['jhira rice\ngundruk\naloo chhola\npickle\n','rice\ndal fry\nrajma masla\npakora\n','roti\ncavage aloo\ndhai kadi\npickles\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay21').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['jhira rice\ngundruk\naloo chhola\npickle\n','rice\ndal fry\nrajma masla\npakora\n','roti\ncavage aloo\ndhai kadi\npickles\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay22').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['jhira rice\ngundruk\naloo chhola\npickle\n','rice\ndal fry\nrajma masla\npakora\n','roti\ncavage aloo\ndhai kadi\npickles\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay23').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['jhira rice\ngundruk\naloo chhola\npickle\n','rice\ndal fry\nrajma masla\npakora\n','roti\ncavage aloo\ndhai kadi\npickles\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay24').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['jhira rice\ngundruk\naloo chhola\npickle\n','rice\ndal fry\nrajma masla\npakora\n','roti\ncavage aloo\ndhai kadi\npickles\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay25').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['jhira rice\ngundruk\naloo chhola\npickle\n','rice\ndal fry\nrajma masla\npakora\n','roti\ncavage aloo\ndhai kadi\npickles\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay26').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['jhira rice\ngundruk\naloo chhola\npickle\n','rice\ndal fry\nrajma masla\npakora\n','roti\ncavage aloo\ndhai kadi\npickles\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay27').innerText = menu[ran];  
+})
+
+
+
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['jhira rice\ngundruk\naloo chhola\npickle\n','rice\ndal fry\nrajma masla\npakora\n','roti\ncavage aloo\ndhai kadi\npickles\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay21').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['jhira rice\ngundruk\naloo chhola\npickle\n','rice\ndal fry\nrajma masla\npakora\n','roti\ncavage aloo\ndhai kadi\npickles\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay22').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['jhira rice\ngundruk\naloo chhola\npickle\n','rice\ndal fry\nrajma masla\npakora\n','roti\ncavage aloo\ndhai kadi\npickles\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay23').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['jhira rice\ngundruk\naloo chhola\npickle\n','rice\ndal fry\nrajma masla\npakora\n','roti\ncavage aloo\ndhai kadi\npickles\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay24').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['jhira rice\ngundruk\naloo chhola\npickle\n','rice\ndal fry\nrajma masla\npakora\n','roti\ncavage aloo\ndhai kadi\npickles\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay25').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['jhira rice\ngundruk\naloo chhola\npickle\n','rice\ndal fry\nrajma masla\npakora\n','roti\ncavage aloo\ndhai kadi\npickles\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay26').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['jhira rice\ngundruk\naloo chhola\npickle\n','rice\ndal fry\nrajma masla\npakora\n','roti\ncavage aloo\ndhai kadi\npickles\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay27').innerText = menu[ran];  
+})
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['rice\ndal makhani\ngreen chutney\nice cream\n','veg biriyani\ndal tadka\naloo lauki\nsalad\n','rice\nchana dal\nchicken\nchutney\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay31').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['rice\ndal makhani\ngreen chutney\nice cream\n','veg biriyani\ndal tadka\naloo lauki\nsalad\n','rice\nchana dal\nchicken\nchutney\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay32').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['rice\ndal makhani\ngreen chutney\nice cream\n','veg biriyani\ndal tadka\naloo lauki\nsalad\n','rice\nchana dal\nchicken\nchutney\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay33').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['rice\ndal makhani\ngreen chutney\nice cream\n','veg biriyani\ndal tadka\naloo lauki\nsalad\n','rice\nchana dal\nchicken\nchutney\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay34').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['rice\ndal makhani\ngreen chutney\nice cream\n','veg biriyani\ndal tadka\naloo lauki\nsalad\n','rice\nchana dal\nchicken\nchutney\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay35').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['rice\ndal makhani\ngreen chutney\nice cream\n','veg biriyani\ndal tadka\naloo lauki\nsalad\n','rice\nchana dal\nchicken\nchutney\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay36').innerText = menu[ran];  
+})
+document.addEventListener('DOMContentLoaded', function(event) {
+	var menu = ['rice\ndal makhani\ngreen chutney\nice cream\n','veg biriyani\ndal tadka\naloo lauki\nsalad\n','rice\nchana dal\nchicken\nchutney\n','roti\ncavage aloo\ndhai kadi\npickles\n']
+	var ran = Math.floor(Math.random() *(menu.length));
+	document.querySelector('#quoteDisplay37').innerText = menu[ran];  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
